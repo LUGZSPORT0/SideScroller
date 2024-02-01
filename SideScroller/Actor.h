0ece6a555f31bbe93055916ac3fd7117386d8f19
LUGZSPORT0 <arjean85@gmail.com>
2024-02-01 01:56:42 -0500

    added Math class, and updated Actor class

--- SideScroller/Actor.h
+++ SideScroller/Actor.h
@@ -1,10 +1,26 @@
 #pragma once
+#include <vector>
+#include "Math.h"
 class Actor
 {
 public:
-	// Called every frame to update the Actor
-	virtual void Update(float deltaTIme);
-	// Called every frame to draw the Actor
-	virtual void Draw();
-};
+	enum State
+	{
+		EActive,
+		EPaused,
+		EDead
+	};
+	// Constructor/destructor
+	Actor(class Game* game);
+	virtual ~Actor();
 
+	// Update funciton called from Game (not overridable)
+	void Update(float deltaTime);
+	// Updates all the coponents attached to the actor (not overridable)
+	void UpdateComponents(float deltaTime);
+	// Any actor-specific update code (overridable)
+	virtual void UpdateActor(float deltaTime);
+
+	// Getters/setters
+	const Vector2& GetPosition() const { return mPosition;  }
+};
