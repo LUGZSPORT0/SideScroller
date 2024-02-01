f9ef00ed91b3760d6278547b7eee934da523fe29
LUGZSPORT0 <arjean85@gmail.com>
2024-02-01 02:06:54 -0500

    completed Actor class

--- SideScroller/Actor.h
+++ SideScroller/Actor.h
@@ -22,5 +22,29 @@ public:
 	virtual void UpdateActor(float deltaTime);
 
 	// Getters/setters
-	const Vector2& GetPosition() const { return mPosition;  }
+	const Vector2 &GetPosition() const { return mPosition; }
+	void SetPosition(const Vector2& pos) { mPosition = pos; }
+	float GetScale() const { return mScale; }
+	void SetScale(float scale) { mScale = scale; }
+	float GetRotation() const { return mRotation; }
+	void SetRotation(float rotation) { mRotation = rotation; }
+
+	State GetState() const { return mState; }
+	void SetState(State state) { mState = state; }
+
+	class Game* GetGame() { return mGame; }
+
+	// Add/remove components
+	void AddComponent(class Component* component);
+	void RemoveComponent(class Component* component);
+private:
+	// Actor's state
+	State mState;
+	
+	Vector2 mPosition;
+	float mScale;
+	float mRotation;
+
+	std::vector<class Component*> mComponents;
+	class Game* mGame;
 };
