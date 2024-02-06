04e379136c906f5cc91a8951008cd6bef5aaa10a
LUGZSPORT0 <arjean85@gmail.com>
2024-02-06 01:40:48 -0500

    updated UpdateGame and add constructor and few variables

--- SideScroller/Game.cpp
+++ SideScroller/Game.cpp
@@ -3,6 +3,14 @@
 #include <algorithm>
 #include "Actor.h"
 
+Game::Game()
+:mWindow(nullptr)
+,mRenderer(nullptr)
+,mIsRunning(true)
+,mUpdatingActors(false)
+{
+
+}
 
 void Game::AddActor(Actor* actor)
 {
@@ -31,5 +39,49 @@ void Game::RemoveActor(Actor* actor)
 		// Swap to end of vector and pop off (avoid erase copies)
 		std::iter_swap(iter, mActors.end() - 1);
 		mActors.pop_back();
+	}
+}
+
+void Game::UpdateGame()
+{
+	// Compute delta time 
+	// Wait until 16ms has elapsed since last frame
+	while (!SDL_TICKS_PASSED(SDL_GetTicks(), mTicksCount + 16))
+		;
+	float deltaTime = (SDL_GetTicks() - mTicksCount) / 1000.0f;
+	if (deltaTime > 0.05f)
+	{
+		deltaTime = 0.05f;
+	}
+	mTicksCount = SDL_GetTicks();
+
+	// Update all actors
+	mUpdatingActors = true;
+	for (auto actor : mActors)
+	{
+		actor->Update(deltaTime);
+	}
+	mUpdatingActors = false;
+
+	// Move any pending actors to mActors
+	for (auto pending : mPendingActors)
+	{
+		mActors.emplace_back(pending);
+	}
+	mPendingActors.clear();
+
+	// Add any dead actors to a temp vector
+	std::vector<Actor*> deadActors;
+	for (auto actor : mActors)
+	{
+		if (actor->GetState() == Actor::EDead)
+		{
+			deadActors.emplace_back(actor);
+		}
+	}
+	// Delete dead actors (which removes them from mActors)
+	for (auto actor : deadActors)
+	{
+		delete actor;
 	}
 }
--- SideScroller/Game.h
+++ SideScroller/Game.h
@@ -15,10 +15,17 @@ public:
 	void RemoveActor(class Actor* actor);
 
 private:
+	void UpdateGame();
+
 	// All the actors in the game
 	std::vector<class Actor*> mActors;
 	// Any pending actors
 	std::vector<class Actor*> mPendingActors;
+
+	SDL_Window* mWindow;
+	SDL_Renderer* mRenderer;
+	Uint32 mTicksCount;
+	bool mIsRunning;
 	// Track if we're updating actors right now
 	bool mUpdatingActors;
 
